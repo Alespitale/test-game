0de4a734e100780f65cdcd0552003bd685c5338c
AleSpitale <alespitale6@gmail.com>
2022-07-22 14:14:06 -0300

    level 1 and 2

--- src/level1/level1.js
+++ src/level1/level1.js
@@ -1,173 +1,126 @@
-
 // En los próximos problemas, deberás completar la función.
 // Todo tu código irá dentro de las llaves de la función.
 // Asegúrate que usas "return" cuando la consola te lo pida.
 // Pista: "console.log()" NO fucionará.
 // No cambies los nombres de las funciones.
 
-export function devolverString() {
-  // Tu código:
-
-  return;  
+export function devolverString(str) {
+  return str.toString();
 }
 
-export function suma() {
- // Tu código:
-
-  return;  
+export function suma(x, y) {
+  return x + y;
 }
 
-export function resta() {
- // Tu código:
-
-  return;
+export function resta(x, y) {
+  return x - y;
 }
 
-export function multiplica() {
-  // Tu código:
-
-  return;
+export function multiplica(x, y) {
+  return x * y;
 }
 
-export function divide() { 
-  // Tu código:
-
-  return;
+export function divide(x, y) {
+  return x / y;
 }
 
-export function sonIguales() {  
-  // Tu código:
-
-  return;  
+export function sonIguales(x, y) {
+  return x === y;
 }
 
-export function tienenMismaLongitud() {  
-  // Tu código:
-
-  return;
+export function tienenMismaLongitud(x, y) {
+  return x.length === y.length;
 }
 
-export function menosQueNoventa() {  
-  // Tu código:
-
-  return;
+export function menosQueNoventa(x) {
+  return x < 90;
 }
 
-export function mayorQueCincuenta() {  
-  // Tu código:
-
-  return;
+export function mayorQueCincuenta(x) {
+  return x > 50;
 }
 
-export function obtenerResto() {  
-  // Tu código:
-
-  return;
+export function obtenerResto(x, y) {
+  return x % y;
 }
 
-export function esPar() {
-  // Tu código:
-
-  return;
+export function esPar(x) {
+  return x % 2 === 0;
 }
 
-export function esImpar() {  
-  // Tu código:
-
-  return;
+export function esImpar(x) {
+  return x % 2 !== 0;
 }
 
-export function elevarAlCuadrado() {  
-  // Tu código:
-
-  return;
+export function elevarAlCuadrado(x) {
+  return x * x;
 }
 
-export function elevarAlCubo() { 
-  // Tu código:
-
-  return;
+export function elevarAlCubo(x) {
+  return x * x * x;
 }
 
-export function elevar() {  
-  // Tu código:
-
-  return;
+export function elevar(x, y) {
+  return x ** y;
 }
 
-export function redondearNumero() {  
-  // Tu código:
-
-  return;
+export function redondearNumero(x) {
+  return Math.round(x);
 }
 
-export function redondearHaciaArriba() { 
-  // Tu código:
-
-  return;
+export function redondearHaciaArriba(x) {
+  return Math.ceil(x);
 }
 
-export function numeroRandom() {  
-  // Tu código:
-
-  return;
+export function numeroRandom() {
+  return Math.random();
 }
 
-export function esPositivo() {
-  // Tu código:
-  
-  return;
+export function esPositivo(x) {
+  if (x > 0) return "Es positivo";
+  else if (x < 0) return "Es negativo";
+  else return false;
 }
 
-export function agregarSimboloExclamacion() {  
-  // Tu código:
-
-  return;
+export function agregarSimboloExclamacion(str) {
+  return str + "!";
 }
 
-export function combinarNombres() {
-  // Tu código:
-
-  return;
+export function combinarNombres(str1, str2) {
+  return str1 + " " + str2;
 }
 
-export function obtenerSaludo() {
-  // Tu código:
-
-  return;
+export function obtenerSaludo(str) {
+  return "Hola " + str + "!";
 }
 
-export function obtenerAreaRectangulo() {  
-  // Tu código:
-
-  return;
+export function obtenerAreaRectangulo(alto, ancho) {
+  return alto * ancho;
 }
 
-
-export function retornarPerimetro(){
-  // Tu código:
-
-  return;
+export function retornarPerimetro(lado) {
+  return lado * 4;
 }
 
-
-export function areaDelTriangulo(){
-  // Tu código:
-
-  return;
+export function areaDelTriangulo(base, altura) {
+  return (base * altura) / 2;
 }
 
-
-export function deEuroAdolar(){
-  //Supongamos que 1 euro equivale a 1.20 dólares.
-  // Tu código:
-
-  return;
+export function deEuroAdolar(euro) {
+  return euro * 1.20;
 }
 
-
-export function esVocal(){
-  // Tu código:
-
-  return;
+export function esVocal(letra) {
+  const array = ["a", "e", "i", "o", "u"];
+  if (
+    letra === "a" ||
+    letra === "e" ||
+    letra === "i" ||
+    letra === "o" ||
+    letra === "u"
+  ) {
+    return "Es vocal";
+  } else {
+    return "Dato incorrecto";
+  }
 }
--- src/level2/level2.js
+++ src/level2/level2.js
@@ -1,94 +1,138 @@
-// No cambies los nombres de las funciones.
-
 export function obtenerMayor(x, y) {
-  // "x" e "y" son números enteros (int).
-  // Tu código:
-
-  return;
+  if (x > y) {
+    return x;
+  }
+  return y;
 }
 
 export function mayoriaDeEdad(edad) {
-  // Tu código:
-
-  return;
+  if (edad >= 18) {
+    return "Allowed";
+  }
+  return "Not allowed";
 }
-  
+
 export function conection(status) {
-  //Recibimos un estado de conexión de un usuario representado por un valor numérico. 
-  // Tu código:
-
-  return;
+  switch (status) {
+    case 1:
+      return "Online";
+    case 2:
+      return "Away";
+    case 3:
+      return "Offline";
+    default:
+      return "Offline";
+  }
 }
 
-export function saludo(idioma) {  
-  // Tu código:
-  
-  return;
+export function saludo(idioma) {
+  switch (idioma) {
+    case "aleman":
+      return "Guten Tag!";
+    case "ingles":
+      return "Hello!";
+    case "mandarin":
+      return "Ni Hao!";
+    default:
+      return "Hola!";
+  }
 }
 
-export function colors(color) {  
-  //Usar el statement Switch.
-  // Tu código:
- 
+export function colors(color) {
+  switch (color) {
+    case "blue":
+      return "This is blue";
+    case "red":
+      return "This is red";
+    case "green":
+      return "This is green";
+    case "orange":
+      return "This is orange";
+    default:
+      return "Color not found";
+  }
 }
 
 export function esDiezOCinco(numero) {
-  // Tu código:
-
-  return;
+  if (numero === 10 || numero === 5) {
+    return true;
+  }
+  return false;
 }
 
-export function estaEnRango(numero) {  
-  // Tu código:
-
-  return;
+export function estaEnRango(numero) {
+  if (numero > 20  && numero < 50) {
+    return true;
+  }
+  return false;
 }
 
 export function esEntero(numero) {
-  // Tu código:
-
-  return;
+  if (numero % 1 === 0) {
+    return true;
+  }
+  return false;
 }
 
 export function fizzBuzz(numero) {
-  // Tu código:
- 
-  return;
+  let msg = "";
+  if (!(numero % 3)) {
+    msg += "fizz";
+  }
+  if (!(numero % 5)) {
+    msg += "buzz";
+  }
+  return msg || numero;
 }
 
 export function operadoresLogicos(num1, num2, num3) {
-  //La función recibe tres números distintos. 
-  // Tu código:
-  
-  return;
+  if (num1 === 0 || num2 === 0 || num3 === 0) {
+    return "Error";
+  }
+  if (num1 < 0 || num2 < 0 || num3 < 0) {
+    return "Hay negativos";
+  }
+  if (num1 > num2 && num1 > num3) {
+    return "Número 1 es mayor y positivo";
+  }
+  if (num3 > num1 && num3 > num2) {
+    return num3 + 1;
+  }
+  return false;
 }
 
 export function esPrimo(numero) {
-  /// Tu código:
-
-  return;  
+  if (numero === 1 || numero === 0) {
+    return false;
+  }
+  for (let i = 2; i < numero; i++) {
+    if (numero % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
-export function esVerdadero(valor){
-  // Tu código:
-
-  return;
+export function esVerdadero(valor) {
+  if (valor) {
+    return "Soy verdadero";
+  }
+  return "Soy falso";
 }
 
-export function tablaDelSeis(){
-  // Tu código:
-
-  return;
+export function tablaDelSeis() {
+  return [...Array(11).keys()].map((i) => i * 6);
 }
 
-export function tieneTresDigitos(numero){
-  // Tu código:
-
-  return ;  
+export function tieneTresDigitos(numero) {
+  return numero.toString().length === 3;
 }
 
-export function doWhile(numero) {  
-  //Usar el bucle do ... while.
-  
-  return;
+export function doWhile(numero) {
+  let i = 0;
+  do {
+    numero += 5;
+    i++;
+  } while (i < 8);
+  return numero;
 }
